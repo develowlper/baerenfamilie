07853ddfe5a70debfe6a7fe759cfdd28c13490b5
develowlper <dev@develowlper.de>
2021-01-07 23:59:28 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "baerenfamilie",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
