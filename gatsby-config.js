fca26badcff0579875d2282ec2f1175ded858ea1
develowlper <dev@develowlper.de>
2021-01-08 02:36:30 +0100

    fix: gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: "baerenfamilie",
+    title: 'baerenfamilie',
+    siteUrl: 'https://baerenfamilie.wedding',
   },
   plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
   ],
 };
